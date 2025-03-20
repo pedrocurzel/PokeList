f489faaecf6e3956aaef5ce14df5b1a0dd49ee5e
pedrocurzel <67026810+pedrocurzel@users.noreply.github.com>
2025-03-20 13:49:52 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PokeList',
+  webDir: 'www'
+};
+
+export default config;
